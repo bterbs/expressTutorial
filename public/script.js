cb54ea58e2ead5772f47caea671f4ebb8166fbfc
bterbs <brittanyterbush@gmail.com>
2017-10-05 15:58:22 -0700

    use script file to start server with express and updated styling of css

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
 const express = require('express');
-const http = require('http');
 
-console.log('javascript is functioning!');
+const app = express();
+const port = 8081;
+
+// to start a server, call listen on app and pass port as argument
+// you can also pass a callback function so you know it is working.
+app.listen(port, function() {
+  console.log('server success');
+});
+
+// The code below tells express to use the files in the public folder.
+app.use(express.static('./public'));
